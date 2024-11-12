f07c08e8fbd514b2753200d68969a5cfe1f04f02
Chris Apps <chrisapps@Chriss-MacBook-Air.local>
2024-11-12 11:09:24 +1000

    config.py file added

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# DATABASE_URL=
+
+# FLASK_APP=main
+# FLASK_ENV=development
+# FLASK_DEBUG=True
+
 @app.route("/")
 def hello_world():
     return "Hello world!"
